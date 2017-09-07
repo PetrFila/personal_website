e2182000b9887f34581709a229f7b1aa75c8f547
Petr Fila <pet.fila@gmail.com>
2017-09-07 13:18:43 +1000

    message form fully working

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails', '~> 5.1.3'
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'dotenv-rails', groups: [:development, :test]
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Bootstrap for styling
--- app/controllers/one_for_all_controller.rb
+++ app/controllers/one_for_all_controller.rb
@@ -1,5 +1,22 @@
 class OneForAllController < ApplicationController
 
-  @headers = ["About", "Skills", "Projects","Contact"]
+  def new
+    @contact = Contact.new
+  end
 
+  def create
+    
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = "Cannot send message."
+      render :new
+    end
+  end
+
+  def complete
+    @contact = Contact.new
+  end
 end
